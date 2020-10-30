9360c8e6cb248d8b3ffb898183477a7fc6ce3018
Xiaozhuoer <1225691169@qq.com>
2020-10-30 11:19:49 +0800

    update test1_8

---
+++ test1_8.cpp
@@ -0,0 +1,49 @@
+/*
+--------------------------------------------------------
+
+   练习1.8
+        1.4节的switch语句让我们得以根据用户答错的次数提供
+    不同的安慰语句。请以array储存四种不同的字符串信息，并
+    以用户答错次数作为array的索引值，以此方式来显示安慰语
+    句
+
+--------------------------------------------------------
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int seq_size = 4;
+string pell_seq[seq_size] = {
+    "Pull yourself together \n",
+    "Keep your chin up \n" ,
+    "Do not be so miserable \n" ,
+    "It will be ok \n" ,
+};
+
+int main(){
+    char  error_times = 0;
+    while(error_times != 'q'){
+        cout << "Please enter error times(1 ~ 4) (q to quit): "  ;
+        cin  >> error_times ;
+        //error_times - '0' 是为了char数字转int
+        switch (error_times - '0' - 1){
+            case 0:
+                cout << pell_seq[0];
+                break;
+            case 1:
+                cout << pell_seq[1];
+                break;
+            case 2:
+                cout << pell_seq[2];
+                break;
+            case 3:
+                cout << pell_seq[3];
+                break;
+            default:
+                break;
+        }
+    }
+}
